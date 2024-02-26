30f59b307de3cf92b8217e036e4ef1b5765d72da
Chirag-16 <chiragjp512@gmail.com>
2024-02-26 23:43:26 +0530

    created url shortner

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const { connectToMongoDB } = require("./connect");
+const urlRoute = require("./routes/url");
+const URL = require("./models/url");
+
+const app = express();
+const PORT = 3000;
+
+connectToMongoDB("mongodb+srv://chiragjp512:12345678910@cluster0.ofbfiir.mongodb.net/ID_Shortner").then(() =>
+  console.log("Mongodb connected")
+);
+
+app.use(express.json());
+
+app.use("/url", urlRoute);
+
+app.get("/:shortId", async (req, res) => {
+  const shortId = req.params.shortId;
+  const entry = await URL.findOneAndUpdate(
+    {
+      shortId,
+    },
+    {
+      $push: {
+        visitHistory: {
+          timestamp: Date.now(),
+        },
+      },
+    }
+  );
+  res.redirect(entry.redirectURL);
+});
+
+app.listen(PORT, () => console.log(`Server Started at PORT:${PORT}`));
